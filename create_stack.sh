521e1557acaa327253feb2c1d01735e2322e19b1
harryjung <harryjung0330@gmail.com>
2023-03-25 17:13:06 +0900

    added database

--- create_stack.sh
+++ create_stack.sh
@@ -10,3 +10,10 @@ aws cloudformation create-stack --template-body file://web_servers.yaml --capabi
 aws cloudformation update-stack --template-body file://web_servers.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=LoadBalancerSubnets,ParameterValue="subnet-05a0919a70315125f\,subnet-0c31e2bae947418bb" \
  ParameterKey=WebServerSubnets,ParameterValue="subnet-04aee95a6c7e6813e\,subnet-0cee9f42b1b0ca063" ParameterKey=VPC,ParameterValue="vpc-0d084a9cfe8adb5b1"\
   ParameterKey=BastionServerSecurityGroup,ParameterValue=sg-06bcb4f3c63436922 ParameterKey=SSHKeyName,ParameterValue=spring-web-server --stack-name asg-example
+
+
+aws cloudformation create-stack --template-body file://RDS.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=DBMasterUserName,ParameterValue="admin" \
+ ParameterKey=DBMasterPassword,ParameterValue=helloworld! ParameterKey=DBSubnets,ParameterValue="subnet-02e90cf7c880a1913\,subnet-0c219a46f47b76a45" ParameterKey=VpcId,ParameterValue="vpc-0d084a9cfe8adb5b1"\
+ ParameterKey=BastionSecGroup,ParameterValue=sg-06bcb4f3c63436922 ParameterKey=ClientSecGroup,ParameterValue=sg-0e516b236a277c345 --stack-name rds-example
+
+
