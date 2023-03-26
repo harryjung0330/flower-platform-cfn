e18c8b39e23dbd7391e8ffcb87dfbc15c3f9ec8a
harryjung <harryjung0330@gmail.com>
2023-03-26 17:48:58 +0900

    deleted example.yaml

--- non-cfn/create_stack.sh
+++ non-cfn/create_stack.sh
@@ -1,7 +1,7 @@
 aws cloudformation create-stack --stack-name <stack-name> --template-body file://<file-path> --parameters ParameterKey=<param-key>,ParameterValue=<param-value> --capabilities CAPABILITY_NAMED_IAM
 
 #example
-aws cloudformation create-stack --template-body file://vpc.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name test-env
+aws cloudformation create-stack --template-body file://vpc.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name test-env --timeout-in-minutes 25
 
 aws cloudformation create-stack --template-body file://web_servers.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=LoadBalancerSubnets,ParameterValue="subnet-05a0919a70315125f\,subnet-0c31e2bae947418bb" \
  ParameterKey=WebServerSubnets,ParameterValue="subnet-04aee95a6c7e6813e\,subnet-0cee9f42b1b0ca063" ParameterKey=VPC,ParameterValue="vpc-0d084a9cfe8adb5b1"\
