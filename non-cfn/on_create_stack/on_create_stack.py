71e0a9c697f8d8dae7a27ccec8194ccca82d6d0e
harryjung <harryjung0330@gmail.com>
2023-03-26 14:54:53 +0900

    new on_create_stack zip file

--- non-cfn/on_create_stack/on_create_stack.py
+++ non-cfn/on_create_stack/on_create_stack.py
@@ -5,13 +5,6 @@ from crhelper import CfnResource
 import os
 import boto3
  
-dbEndpoint = os.environ['DB_ENDPOINT']
-S3Bucket = os.environ['BUCKET_NAME']
-ddlKey = os.environ['DDL_KEY']
-dmlKey = os.environ['DML_KEY']
-user= os.environ["USER"]
-password = os.environ["PASSWORD"]
-
 helper = CfnResource(
     json_logging=False,
     log_level='DEBUG',
@@ -26,14 +19,14 @@ def lambda_handler(event, context):
         'body': json.dumps('success')
     }
 
-def getDDL():
+def getDDL(S3Bucket, ddlKey):
     s3 = boto3.resource('s3')
     obj = s3.Object(S3Bucket, ddlKey)
     ddl = obj.get()['Body'].read().decode('utf-8')
     
     return ddl
 
-def getDML():
+def getDML(S3Bucket, dmlKey):
     s3 = boto3.resource('s3')
     obj = s3.Object(S3Bucket, dmlKey)
     dml = obj.get()['Body'].read().decode('utf-8')
@@ -46,19 +39,27 @@ def executeStatements(cursor, sqlList):
         cursor.execute(sql);
     cursor.execute("COMMIT;")
     
-def connectToRds():
-    connection = pymysql.connect(host = dbEndpoint, port = 3306, user=user, passwd=password, charset = 'utf8')
-    cursor = connection.cursor()
     
 @helper.create
 def create(event, context):
     print("Got Create")
+    print(event)
+    
+    properties = event.get('ResourceProperties', {})
+    
+    dbEndpoint = properties.get('DB_ENDPOINT')
+    user = properties.get('USER')
+    password = properties.get('PASSWORD')
+    S3Bucket = properties.get('S3Bucket')
+    ddlKey = properties.get('DDL_KEY')
+    dmlKey = properties.get('DML_KEY')
     
     try:
         connection = pymysql.connect(host = dbEndpoint, port = 3306, user=user, passwd=password, charset = 'utf8')
         cursor = connection.cursor()
-        ddlList = sqlparse.split(getDDL())
-        dmlList = sqlparse.split(getDML())
+        ddlList = sqlparse.split(getDDL(S3Bucket, ddlKey))
+        dmlList = sqlparse.split(getDML(S3Bucket, dmlKey))
+        
         print("ddlList")
         print(ddlList)
         print("dmlList")
